83878221c9fb82feb04379e08264558239223f74
Dima Sorokin <sorokindima36@gmail.com>
2023-06-08 19:05:33 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/Korzinka.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Korzinka.xaml
+    /// </summary>
+    public partial class Korzinka : Window
+    {
+        public bool itog = false;
+        List<Basket> baskets = Entities.GetContext().Basket.ToList();
+        public Korzinka()
+        {
+            InitializeComponent();
+            List.ItemsSource = baskets;
+            var allTypes = Entities.GetContext().Avtors.ToList();
+            allTypes.Insert(0, new Avtors
+            {
+                Avtor = "Пункты выдачи"
+            });
+            combo.ItemsSource = allTypes;
+            combo.SelectedIndex = 0;
+            int c = 0;
+            for (int i = 0; i < baskets.Count; i++)
+            {
+                c += Convert.ToInt32(baskets[i].Books.Cost) * Convert.ToInt32(baskets[i].koll);
+            }
+            ItogCost.Content = Convert.ToString(c + " руб");
+        }
+
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+            Entities.GetContext().Basket.RemoveRange(baskets);
+            Entities.GetContext().SaveChanges();
+            itog = true;
+            Close();
+
+        }
+
+        private void pynkt_Click(object sender, RoutedEventArgs e)
+        {
+           PrintDialog printDialog = new PrintDialog();
+            printDialog.PrintVisual(List, "");
+        }
+
+        private async void Label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            await Task.Delay(100);
+            List<Basket> b = new List<Basket> { new Basket() };
+            List<Basket> b1 = Entities.GetContext().Basket.ToList();
+            b[0] = List.SelectedItem as Basket;
+            for (int i = 0; i < b1.Count; i++)
+            {
+                if (b1[i].ID == b[0].ID)
+                {
+                    b1[i].koll -= 1;
+                }
+            }
+            for (int i = 0; i < b1.Count; i++)
+            {
+                if (b1[i].ID == b[0].ID)
+                {
+                    if (b1[i].koll == 0)
+                    {
+                        Entities.GetContext().Basket.Remove(b1[i]);
+                    }
+                }
+            }
+            Entities.GetContext().SaveChanges();
+            List.ItemsSource = Entities.GetContext().Basket.ToList();
+            b1 = Entities.GetContext().Basket.ToList();
+            int c = 0;
+            for (int i = 0; i < b1.Count; i++)
+            {
+                c += Convert.ToInt32(b1[i].Books.Cost) * Convert.ToInt32(b1[i].koll);
+            }
+            ItogCost.Content = Convert.ToString(c + " руб");
+        }
+
+        private async void Label_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            await Task.Delay(100);
+            List<Basket> b = new List<Basket> { new Basket() };
+            List<Basket> b1 = Entities.GetContext().Basket.ToList();
+            b[0] = List.SelectedItem as Basket;
+            for (int i = 0; i < b1.Count; i++)
+            {
+                if (b1[i].ID == b[0].ID)
+                {
+                    b1[i].koll += 1;
+                }
+            }
+            Entities.GetContext().SaveChanges();
+            List.ItemsSource = Entities.GetContext().Basket.ToList();
+            b1 = Entities.GetContext().Basket.ToList();
+            int c = 0;
+            for (int i = 0; i < b1.Count; i++)
+            {
+                c += Convert.ToInt32(b1[i].Books.Cost) * Convert.ToInt32(b1[i].koll);
+            }
+            ItogCost.Content = Convert.ToString(c + " руб");
+        }
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+      
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Basket> b = Entities.GetContext().Basket.ToList();
+            List.ItemsSource=Entities.GetContext().Books.ToList();
+            if(b.Count > 0 )
+            {
+                int cont = 0;
+                for (int i = 0; i < b.Count; i++)
+                {
+                    cont += Convert.ToInt32(b[i].koll);
+                }
+                kolKorz.Content = Convert.ToString(cont) + " шт.";
+                korz.Visibility=Visibility.Visible;
+                kolKorz.Visibility=Visibility.Visible;
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            List<Basket> b = Entities.GetContext().Basket.ToList();
+            List<Books> books = new List<Books> { new Books() };
+            books[0]=List.SelectedItem as Books;
+            int k = 1;
+            bool pr = false;
+                bool pr1 = false;
+            int p=0;
+            for (int i = 0; i < b.Count; i++)
+            {
+                if (b[i].id_Book == books[0].Id_Product)
+                {
+                    if (books[0].koll> b[i].koll)
+                    {
+                        k = (int)b[i].koll + 1;
+                        pr = true;
+                        p = i;
+                    }
+                    else
+                    {
+                        pr1 = true;
+                        MessageBox.Show("Отсутствует на складе");
+                        break;
+                    }
+                }
+            }
+            if(pr==true)
+            {
+                b[p].koll = k;
+            }
+            else
+            {
+                if(pr1==false)
+                {
+                    var add = new Basket { id_Book = books[0].Id_Product, koll = k };
+                    Entities.GetContext().Basket.Add(add);
+                }
+
+            }
+            Entities.GetContext().SaveChanges();
+            int cont = 0;
+            List<Basket> b1 = Entities.GetContext().Basket.ToList();
+            for (int i = 0; i < b1.Count; i++)
+            {
+                cont += Convert.ToInt32(b1[i].koll);
+            }
+            kolKorz.Content = Convert.ToString(cont) + " шт.";
+            korz.Visibility = Visibility.Visible;
+            kolKorz.Visibility = Visibility.Visible;
+            int c = 0;
+            for (int i = 0; i < b1.Count; i++)
+            {
+                c += Convert.ToInt32(b1[i].Books.Cost) * Convert.ToInt32(b1[i].koll);
+            }
+            MessageBox.Show(Convert.ToString(c));
+        }
+
+        private void korz_Click(object sender, RoutedEventArgs e)
+        {
+            Korzinka korzinka = new Korzinka();
+            korzinka.ShowDialog();
+            if (korzinka.itog == true)
+            {      
+                    kolKorz.Content =  "";
+                    korz.Visibility = Visibility.Collapsed;
+                    kolKorz.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
